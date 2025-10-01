dd5ca49f833bf1cdf4ce821cbc096503bf15ab45
salomeMumladze <salomemumladze775@gmail.com>
2025-10-01 16:19:10 +0400

    job card and link

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\JobBoardController;
 use Illuminate\Support\Facades\Route;
 
+Route::get('', fn() => to_route('jobs.index'));
 
 Route::resource('jobs', JobBoardController::class)
-    ->only(['index']);
+    ->only(['index', 'show']);
