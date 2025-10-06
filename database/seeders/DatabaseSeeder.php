2212c2bc25a40a42440b346ca8b7800525d28b39
salomeMumladze <salomemumladze775@gmail.com>
2025-10-06 13:44:32 +0400

    sing in

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -32,9 +32,9 @@ class DatabaseSeeder extends Seeder
 
         // \App\Models\User::factory(10)->create();
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        \App\Models\User::factory()->create([
+            'name' => 'salome mumladze',
+            'email' => 'salomeMumladze@example.com',
+        ]);
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,25 +1,15 @@
 <?php
 
 use App\Http\Controllers\AuthController;
-use App\Http\Controllers\JobController;
+use App\Http\Controllers\JobBoardController;
 use Illuminate\Support\Facades\Route;
-
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
 
 Route::get('', fn() => to_route('jobs.index'));
 
-Route::resource('jobs', JobController::class)
+Route::resource('jobs', JobBoardController::class)
     ->only(['index', 'show']);
 
 Route::get('login', fn() => to_route('auth.create'))->name('login');
-Route::resource('auth', AuthController::class)
-    ->only(['create', 'store']);
+
+Route::resource('auth', AuthController::class);
+   
