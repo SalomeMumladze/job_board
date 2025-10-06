315460ea922b5aa58c4f3ef0d8cdea38795160e7
salomeMumladze <salomemumladze775@gmail.com>
2025-10-06 13:31:52 +0400

    auth controller

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,25 @@
 <?php
 
-use App\Http\Controllers\JobBoardController;
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\JobController;
 use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
 
 Route::get('', fn() => to_route('jobs.index'));
 
-Route::resource('jobs', JobBoardController::class)
+Route::resource('jobs', JobController::class)
     ->only(['index', 'show']);
+
+Route::get('login', fn() => to_route('auth.create'))->name('login');
+Route::resource('auth', AuthController::class)
+    ->only(['create', 'store']);
